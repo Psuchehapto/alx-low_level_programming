3ecabb5da0450b07fe00caebd77debc58a437350
psuchehapto <akfrano@gmail.com>
2022-09-08 19:56:23 -0700

    Done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Print the program.
+ * main - Print the program
  *
- * this program prints "Programming is like a multilingual puzzle 
- * Return: 0
+ * this program prints "Programming is like a multilingual
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,18 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-	int inte;
-	long int lint;
-	long long int llint;
-	float flo;
-	char cha;
-
-	printf("Size of a char: %i byte(s)\n", sizeof(char));
-	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	Printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	int x;
+	char z;
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	Printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
