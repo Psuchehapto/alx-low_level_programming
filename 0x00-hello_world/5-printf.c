d88a6fbb2e43a4b142beef657210bd8936e157de
psuchehapto <akfrano@gmail.com>
2022-09-08 19:24:55 -0700

    x

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point for the program.
+ * main - Print the program.
  *
- * Return: 0 (Success)
+ * this program prints "Programming is like a multilingual puzzle 
+ * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
+	return (0);
 }
