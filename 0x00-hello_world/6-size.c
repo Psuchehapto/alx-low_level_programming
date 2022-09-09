d71273b9d6eb5ef0fe4a02fa322cab1265cd2632
psuchehapto <akfrano@gmail.com>
2022-09-08 20:32:01 -0700

    Slepless Night

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
-#include <stdio.hi>
-
-int main(0)
-{
-  int x;
-  char z;
-
-  printf("The size of a char: %d byte(s)\n", sizeof(char));
-  printf("The size of an int: %d byte(s)\n", sizeof(int));
-  printf("The size of a long int: %d byte(s)\n", sizeof(long int));
-  printf("The size of a long long int: %d byte(s)\n", sizeof(long long int));
-  printf("The size of a float: %d byte(s)\n", sizeof(float));
-  return 0;
-}
+ #include <stdio.h>
+ 
+ /**
+  * main - prints the size of various types
+  * Return: 0 if exited properly, non-zero otherwise
+  */
+ int main(void)
+ {
+     printf("Size of a char: %d byte(s)\n", sizeof(char));
+     printf("Size of an int: %d byte(s)\n", sizeof(int));
+     printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+     printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+     printf("Size of a float: %d byte(s)\n", sizeof(float));
+     return (0);
+ }
