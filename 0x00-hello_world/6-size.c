38904f85a58a64c76b77005629ead4bf123cf92c
psuchehapto <akfrano@gmail.com>
2022-09-08 21:36:30 -0700

    tox

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
  #include <stdio.h>
  
  /**
+  *
   * main - Entry point
   *
   * Return: 0 if exited properly, non-zero otherwise
@@ -9,7 +10,7 @@
  {
      printf("Size of a char: %lu byte(s)\n", sizeof(char));
      printf("Size of an int: %lu byte(s)\n",  sizeof(int));
-     printf("SIize of a long int: %lu byte(s)\n", sizeof(long int));
+     printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
      printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
      printf("Size of a float: %lu byte(s)\n", sizeof(float));
      return (0);
