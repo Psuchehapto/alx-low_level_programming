ce1719db3efa9a83129e3b852e8138301d449476
psuchehapto <akfrano@gmail.com>
2022-09-08 18:56:34 -0700

    X

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,8 @@
 /**
  * main - Entry point for the program.
  *
- * Return: 0 (Success)*/
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
