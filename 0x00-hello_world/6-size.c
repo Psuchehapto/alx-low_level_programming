ce9130591e84ffb21c2f257ecb3ebc39ca9541ef
psuchehapto <akfrano@gmail.com>
2022-09-08 18:20:32 -0700

    Alx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio>
 /**
- * main is the entry point for the program.
+ * main - is the entry point for the program.
  *
- * Return:0 if no errors, non-zero value if errors.
+ * Return: 0 if no errors, non-zero value if errors.
  */
 int main(void)
 {
