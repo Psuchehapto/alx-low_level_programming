614ff9eedb2b47816e8529ee1c30a9c9b6f8b190
psuchehapto <akfrano@gmail.com>
2022-09-08 17:29:18 -0700

    Lagitte

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main is the entry point for the program.
+ *
+ * Return:0 if no errors, non-zero value if errors.
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	Printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", szeof(float));
+	return (0);
+}
